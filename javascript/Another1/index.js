cc6f44e9fc3d651166a92cfcaf1109d2e7edc50a
dev0xeb <clintonayomide96@gmail.com>
2024-09-19 09:07:50 +0100

    commit

---
+++ javascript/Another1/index.js
@@ -0,0 +1,19 @@
+let array = [1, 2, 3, "homie", true];
+// console.log(array)
+
+let newArray = new Array(3);
+newArray.push(2);
+newArray.push(3);
+newArray.push(4);
+newArray.push(5);
+
+// console.log(newArray)
+let result = Array.from("semicolon")
+// console.log(result)
+
+let answer = Array.of(1,2,3,4,5,6,7,8,9,8,7,6)
+// console.log(answer)
+
+let arrayOfNumbers =[1,2,3,4,5,67];
+let slice = arrayOfNumbers.slice(2, 40 )
+console.log(slice)
