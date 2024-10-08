2d85dd6e82dc221967dca8ea4de7de8257503174
dev0xeb <clintonayomide96@gmail.com>
2024-10-08 19:06:23 +0100

    commit

--- javascript/Another1/Rectangle.js
+++ javascript/Another1/Rectangle.js
@@ -1,15 +1,27 @@
+import { Shape } from './Shape.js';
 class Rectangle extends Shape{
-    #length
-    #width
-    constructor(length, width) {
-        super();
-        this.#width = width;
-        this.#length = length;
-        this.calculateArea();
+    constructor(name, length, width) {
+        super(name);
+        this.width = width;
+        this.length = length;
     }
 
     calculateArea() {
-        this.area = this.length * this.length;
+        this.area = this.length * this.width;
         return this.area
     }
+
+    isSquare(){
+        return this.length === this.width
+    }
 }
+
+const myRectangle = new Rectangle("Rectangle", 5, 10)
+console.log("shape name ", myRectangle.getName())
+console.log(myRectangle.calculateArea())
+console.log(myRectangle.isSquare())
+
+const mySquare = new Rectangle("Square", 10, 10)
+console.log(mySquare.getName())
+console.log(mySquare.calculateArea())
+console.log(mySquare.isSquare())
--- javascript/Another1/Shape.js
+++ javascript/Another1/Shape.js
@@ -1,4 +1,4 @@
-class Shape {
+export class Shape {
     #name = ""
     constructor(name) {
     this.#name = name;
