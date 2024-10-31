63e69902c9f5a25745941d42483c8b27b49d84dc
dev0xeb <clintonayomide96@gmail.com>
2024-10-31 16:46:35 +0100

    commit

--- LandingPage/MovieApp/movie.js
+++ LandingPage/MovieApp/movie.js
@@ -1,6 +1,12 @@
 const API_KEY = "api_key=d59c7b28e92f24b3f428ca732c1463e1"
-const BASE_URL = "https://api.themoviedb.org/3/movie/"
-const API_URL = `${BASE_URL}popular?${API_KEY}`;
+const BASE_URL = "https://api.themoviedb.org/3/"
+const API_URL = `${BASE_URL}movie/popular?${API_KEY}`;
+const SEARCH_MOVIE_URL = `${BASE_URL}search/movie${API_KEY}`;
+const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
+
+const movieMainContainer = document.querySelector(".movieMainContainer");
+console.log(movieMainContainer);
+
 
 console.log(API_URL);
 
@@ -16,7 +22,7 @@ const getMovie = async (url) => {
         const response = await fetch(url)
         const data = await response.json()
         console.log(data.results)
-
+        showMovie(data.results)
     } catch (error) {
         console.log(error);
         
@@ -24,3 +30,41 @@ const getMovie = async (url) => {
 }
 
 getMovie(API_URL)
+
+function showMovie(movies) {
+    movieMainContainer.innerHTML = '';
+
+    movies.forEach(movie =>{
+        const {overview, title, vote_average, poster_path} = movie
+        const movieElement = document.createElement('div');
+        movieElement.classList.add('movie');
+        movieElement.innerHTML = `
+        <div>
+            <img src="${IMAGE_URL}${poster_path}" alt="">
+        </div>
+        <div class="titleRating">
+            <span>${title}</span>
+            <span>${vote_average}</span>
+        </div>
+        <div class="overview">
+            <h2>Overview</h2>
+            <p>${overview} </p>
+        </div>`
+
+        movieMainContainer.appendChild(movieElement)
+    });
+
+}
+
+const searchMovieForm = document.querySelector(`.search`)
+searchMovieForm.addEventListener(`keyup`, (event)=>{
+    event.preventDefault();
+    const inputValue = event.target.value
+    console.log(inputValue);
+    if(inputValue){
+        const searchUrl = SEARCH_MOVIE_URL + "&query=" + inputValue
+        getMovie(searchUrl)
+    } else{
+        getMovie(API_URL)
+    }
+})
